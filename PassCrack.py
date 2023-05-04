c562fdb9b223cb6d6cd071a99324adbd648c2eb3
TheGodSkill <125906953+TheGodSkill@users.noreply.github.com>
2023-05-04 23:29:22 +0200

    Update PassCrack.py

--- PassCrack.py
+++ PassCrack.py
@@ -2,11 +2,11 @@ import hashlib
 
 target_hash = '5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8' # SHA-256 hash of 'password'
 
-# List of potential passwords
-# you can use a list if you want
-passwords = ['password', '123456', 'qwerty', 'letmein', 'admin']
 
-# Loop through each password and hash it for comparison
+with open('rockyou.txt', 'r', encoding='latin-1') as f:
+ passwords = f.read().splitlines()
+
+
 for password in passwords:
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
     if hashed_password == target_hash:
